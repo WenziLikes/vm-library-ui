69c05d03101d3fdec31b85fef7387eb4ab19d9a7
viacheslavmurakhin <slavamyraxin@icloud.com>
2023-02-28 01:11:04 +0100

    add all file ignore, dd import styles

--- src/stories/ButtonVM.stories.tsx
+++ src/stories/ButtonVM.stories.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import "../components/Buttons/ButtonVM.css"
 
 import ButtonVM from "../components/Buttons/ButtonVM"
 
 export default {
-  title: 'Example/ButtonVM',
-  component: ButtonVM,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof ButtonVM>;
+    title: "Example/ButtonVM",
+    component: ButtonVM,
+    argTypes: {
+        backgroundColor: {control: 'color'}
+    },
+} as ComponentMeta<typeof ButtonVM>
 
-const Template: ComponentStory<typeof ButtonVM> = (args:any) => <ButtonVM {...args} />;
+const Template: ComponentStory<typeof ButtonVM> = (args: any) => <ButtonVM {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   label: 'Button',
-  primary: "danger"
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: 'large',
+    label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: 'small',
+    label: 'Button',
 };
